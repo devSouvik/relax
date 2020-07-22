f64e483c4303c45fc3da7aa9569d67c325d0386e
souvikguria98 <souvikguria98@gmail.com>
2020-07-22 10:55:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/relax/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.relax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button rainButton, forestButton, fireButton, galaxyButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        forestButton = findViewById(R.id.forest);
+        rainButton = findViewById(R.id.rain);
+        fireButton = findViewById(R.id.fire);
+        galaxyButton = findViewById(R.id.galaxy);
+
+        forestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, forestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        rainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, rainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        fireButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, fireActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        galaxyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, galaxyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/relax/rainActivity.java
@@ -0,0 +1,139 @@
+package com.example.relax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class rainActivity extends AppCompatActivity {
+
+    private ImageView imagePlayPause;
+    private TextView textCurrentTime, textTotalDuration;
+    private SeekBar playerSeekBar;
+    private MediaPlayer mediaPlayer;
+    private Handler handler = new Handler();
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rain);
+
+        imagePlayPause = findViewById(R.id.imagePlayPause);
+        textCurrentTime = findViewById(R.id.textCurrentTime);
+        textTotalDuration = findViewById(R.id.textTotalDuration);
+        playerSeekBar = findViewById(R.id.playerSeekBar);
+        mediaPlayer = new MediaPlayer();
+
+        playerSeekBar.setMax(100);
+
+        imagePlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){
+                    handler.removeCallbacks(updater);
+                    mediaPlayer.pause();
+                    imagePlayPause.setImageResource(R.drawable.ic_play);
+                } else {
+                    mediaPlayer.start();
+                    imagePlayPause.setImageResource(R.drawable.ic_pause);
+                    updateSeekBar();
+                }
+            }
+        });
+
+        prepareMediaPlayer();
+
+        playerSeekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SeekBar seekbar = (SeekBar) view;
+                int playPosition= (mediaPlayer.getDuration()/100) * seekbar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                textCurrentTime.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                playerSeekBar.setSecondaryProgress(i);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                playerSeekBar.setProgress(0);
+                imagePlayPause.setImageResource(R.drawable.ic_play);
+                textCurrentTime.setText(R.string.zero);
+                textTotalDuration.setText(R.string.zero);
+                mediaPlayer.reset();
+                prepareMediaPlayer();
+            }
+        });
+
+    }
+
+    public void prepareMediaPlayer(){
+        try{
+            mediaPlayer.setDataSource("https://cdn.uploader.win/uploads/user/1096/21-Jul-2020/rain1.mp3");
+            mediaPlayer.prepare();
+            textTotalDuration.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+        } catch (Exception exception) {
+            Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekBar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            textCurrentTime.setText(milliSecondsToTimer(currentDuration));
+        }
+    };
+
+    private void updateSeekBar() {
+        if (mediaPlayer.isPlaying()) {
+            playerSeekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
+            handler.postDelayed(updater,1000);
+        }
+    }
+
+    private String milliSecondsToTimer(long milliSeconds) {
+        String timerString = "";
+        String secondsString;
+        int hours = (int) (milliSeconds / (1000 * 60 * 60));
+        int minutes = (int) (milliSeconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliSeconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+
+        if (hours > 0) {
+            timerString = hours + ":";
+        }
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+        timerString = timerString + minutes + ":" + secondsString;
+        return timerString;
+    }
+    public void onBackPressed() {
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            finish();
+        }
+        super.onBackPressed();
+    }
+}
