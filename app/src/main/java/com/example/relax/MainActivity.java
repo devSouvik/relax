a796b0a363596ee5eab82c84be08c88b369920bc
souvikguria98 <souvikguria98@gmail.com>
2020-07-23 18:34:17 +0530

    added admob

--- app/src/main/java/com/example/relax/MainActivity.java
+++ app/src/main/java/com/example/relax/MainActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button rainButton, forestButton, fireButton, galaxyButton;
--- app/src/main/java/com/example/relax/fireActivity.java
+++ app/src/main/java/com/example/relax/fireActivity.java
@@ -14,6 +14,12 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class fireActivity extends AppCompatActivity {
 
     private ImageView imagePlayPause;
@@ -21,12 +27,23 @@ public class fireActivity extends AppCompatActivity {
     private SeekBar playerSeekBar;
     private MediaPlayer mediaPlayer;
     private Handler handler = new Handler();
+    private AdView mAdView;
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         imagePlayPause = findViewById(R.id.imagePlayPause);
         textCurrentTime = findViewById(R.id.textCurrentTime);
